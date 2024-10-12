72ea89e8d2fb394338f66e3ce7087d394814a97c
karolcymerys <karol.cymerys@gmail.com>
2024-10-12 13:07:43 +0200

    Train procedure

---
+++ main.py
@@ -0,0 +1,56 @@
+from torch.nn import CrossEntropyLoss
+from torch.utils.data import Subset, DataLoader
+from torchvision.datasets import StanfordCars
+from torchvision.transforms import transforms
+
+from dataset.train_val_split import VAL_ITEMS_IDX
+from loss_functions import FocalLoss
+from models.resnet.model import resnet18, resnet34, resnet50, resnet101, resnet152
+from models.vgg.model import VGG11, VGG13, VGG16, VGG19
+from train import train
+
+DEVICE = 'cuda:0'
+BATCH_SIZE = 64
+
+if __name__ == '__main__':
+    train_dataset = StanfordCars(
+        root='./data',
+        split='train',
+        transform=transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.RandomCrop((224, 224)),
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(15),
+            transforms.ToTensor()
+        ])
+    )
+
+    test_dataset = StanfordCars(
+        root='./data',
+        split='test',
+        transform=transforms.Compose([
+            transforms.Resize((256, 256)),
+            transforms.RandomCrop((224, 224)),
+            transforms.ToTensor(),
+        ])
+    )
+
+    val_dataset = Subset(test_dataset, VAL_ITEMS_IDX)
+
+    train_data_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    validation_data_loader = DataLoader(val_dataset, batch_size=16)
+    labels = len(train_dataset.class_to_idx.values())
+
+    model = VGG11(classes=labels).to(DEVICE)
+    model.init_weights()
+
+    model = train(
+        model,
+        train_data_loader,
+        validation_data_loader,
+        CrossEntropyLoss(),
+        'vgg',
+        'vgg11_cross_entropy_loss',
+        device=DEVICE,
+        learning_rate=3e-4
+    )
--- train.py
+++ train.py
@@ -1,26 +1,94 @@
-from torch.utils.data import Subset, DataLoader
-from torchvision.datasets import StanfordCars
-from torchvision.transforms import transforms
+import os
+from typing import Tuple
 
-from dataset.train_val_split import TRAIN_ITEMS_IDX, VAL_ITEMS_IDX
+import torch
+from torch import nn
+from torch.optim import SGD
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.utils.data import DataLoader
+from tqdm import tqdm
 
-DEVICE = 'cuda:0'
-BATCH_SIZE = 256
+
+def train(model: nn.Module,
+          train_data_loader: DataLoader,
+          validation_data_loader: DataLoader,
+          loss_fn: nn.Module,
+          model_name: str,
+          model_suffix: str,
+          epochs: int = 100,
+          learning_rate: float = 1e-2,
+          device: str = 'cpu') -> nn.Module:
+    optimizer = SGD(model.parameters(), lr=learning_rate, momentum=0.9)
+    scheduler = ReduceLROnPlateau(optimizer, mode='min')
+
+    epoch_losses = []
+    for epoch in range(1, epochs + 1):
+        train_epoch_loss = __train(model, train_data_loader, optimizer, loss_fn, device)
+        validation_data_loss, validation_accuracy = __validate(model, validation_data_loader, loss_fn, device)
+        scheduler.step(validation_data_loss)
+        print(
+            f'[{epoch}/{epochs}]\t'
+            f'Train epoch loss: {train_epoch_loss}\t'
+            f'Validation epoch loss: {validation_data_loss}\t'
+            f'Validation accuracy: {validation_accuracy * 100}%')
+
+        torch.save(
+            model.state_dict(),
+            os.path.join('models', model_name, 'weights', f'{model_suffix}_{epoch}.pth')
+        )
+
+        epoch_losses.append(validation_data_loss)
+    return model
 
 
-if __name__ == '__main__':
-    dataset = StanfordCars(
-        root='./data',
-        split='train',
-        transform=transforms.Compose([
-            transforms.Resize((224, 224)),
-            transforms.ToTensor()
-        ])
-    )
+def __train(model: nn.Module,
+            data_loader: DataLoader,
+            optimizer,
+            loss_fn,
+            device: str = 'cpu') -> float:
+    model = model.train()
+    with tqdm(data_loader, total=len(data_loader)) as samples:
+        epoch_loss = 0.0
+        for batch_idx, batch in enumerate(samples, start=1):
+            imgs, targets = batch[0].to(device), batch[1].to(device)
 
-    train_dataset = Subset(dataset, TRAIN_ITEMS_IDX)
-    val_dataset = Subset(dataset, VAL_ITEMS_IDX)
+            outputs = model(imgs)
 
-    train_data_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    validation_data_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE)
-    labels = len(dataset.class_to_idx.values())
+            optimizer.zero_grad()
+            loss = loss_fn(outputs, targets)
+            loss.backward()
+            optimizer.step()
+
+            epoch_loss += loss.item()
+
+            samples.set_postfix({
+                'Epoch loss': epoch_loss / batch_idx
+            })
+
+        return epoch_loss / batch_idx
+
+
+def __validate(model: nn.Module,
+               data_loader: DataLoader,
+               loss_fn,
+               device: str = 'cpu') -> Tuple[float, float]:
+    model = model.eval()
+    with tqdm(data_loader, total=len(data_loader)) as samples:
+        epoch_loss = 0.0
+        epoch_correct_predictions = 0
+        for batch_idx, batch in enumerate(samples, start=1):
+            imgs, targets = batch[0].to(device), batch[1].to(device)
+
+            outputs = model(imgs)
+
+            loss = loss_fn(outputs, targets)
+            epoch_loss += loss.item()
+
+            predicted_class = torch.argmax(nn.functional.log_softmax(outputs, dim=1), dim=1)
+            epoch_correct_predictions += (predicted_class == targets).int().sum().item()
+
+            samples.set_postfix({
+                'Epoch loss': epoch_loss / batch_idx
+            })
+
+        return epoch_loss / batch_idx, epoch_correct_predictions / len(data_loader.dataset)
